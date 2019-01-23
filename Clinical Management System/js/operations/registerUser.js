03ec51e25b3986ac2cf95d190990f6227668416a
ThilinaPrasad <thilinaprasad.15@cse.mrt.ac.lk>
2019-01-23 21:08:01 +0530

    Admin pages added with admin bsb template

--- Clinical Management System/js/operations/registerUser.js
+++ Clinical Management System/js/operations/registerUser.js
@@ -3,21 +3,21 @@ let isEmailValid = false;
 let isPassValid = false;
 
 // email validations
-function email(email,id) {
-    $.get("../../php/patients/getPatients.php?column=email&data="+email, function(data, status){
-        if(data != 'false'){
+function email(email, id) {
+    $.get("../../php/patients/getPatients.php?column=email&data=" + email, function (data, status) {
+        if (data != 'false') {
             isEmailValid = false;
-            errorToggle(id,"Email already exits!",true);
-        }else{
+            errorToggle(id, "Email already exits!", true);
+        } else {
             isEmailValid = true;
-            errorToggle(id,"Looks Good!",false);
+            errorToggle(id, "Looks Good!", false);
         }
     });
 }
 
 // Password validation
 function cmf_password(id) {
-    if($('#cmf-password-input').val()) {
+    if ($('#cmf-password-input').val()) {
         if ($('#cmf-password-input').val() != $("#password-input").val()) {
             isPassValid = false;
             errorToggle(id, "Password mismatched!", true);
@@ -29,18 +29,18 @@ function cmf_password(id) {
 }
 
 // Error display function
-function errorToggle(id,text_input,isError){
+function errorToggle(id, text_input, isError) {
     var element = $(id);
-    var input_element = $(id+"-input");
-    var err_element = $(id+"-input-error");
-    if(isError){
+    var input_element = $(id + "-input");
+    var err_element = $(id + "-input-error");
+    if (isError) {
         err_element.text(text_input);
         err_element.show();
         input_element.removeClass('input-type-1');
         input_element.addClass('input-type-1-error');
         element.removeClass('bg-color-type-1 border-type-1');
         element.addClass("bg-color-type-1-error border-type-1-error");
-    }else {
+    } else {
         err_element.hide();
         input_element.removeClass('input-type-1-error');
         input_element.addClass('input-type-1');
@@ -51,12 +51,12 @@ function errorToggle(id,text_input,isError){
 }
 
 // add user
-function reg_user(){
-    if(isEmailValid && isPassValid) {
+function reg_user() {
+    if (isEmailValid && isPassValid) {
         $("#register-btn").removeClass("btn-type-1-error");
         $("#register-btn").addClass("btn-type-1");
         $("#register-btn-input-error").hide();
-        $('#register-btn').fadeOut('fast','linear',function(){
+        $('#register-btn').fadeOut('fast', 'linear', function () {
             $('#register-spinner').fadeIn();
         });
 
@@ -70,23 +70,23 @@ function reg_user(){
         $.post("../../php/patients/userRegistration.php",
             {
                 name: name,
-                age:age,
+                age: age,
                 email: email,
-                telephone:telephone,
-                address:address,
-                password:password
+                telephone: telephone,
+                address: address,
+                password: password
             },
 
-            function(result){
-                if(result==1){
+            function (result) {
+                if (result == 1) {
                     // ************************** redirect to dashboard here ***************************
                     alert("Redirect to dashboard here!");
-                }else{
+                } else {
                     $.confirm({
                         theme: 'modern',
                         icon: 'fa fa-exclamation-circle',
                         title: 'Error !',
-                        content: "Error happened. Please try again!" ,
+                        content: "Error happened. Please try again!",
                         draggable: true,
                         animationBounce: 2.5,
                         type: 'red',
@@ -95,8 +95,8 @@ function reg_user(){
                             Delete: {
                                 text: 'Try Again',
                                 btnClass: 'btn-danger',
-                                action : function () {
-                                    $('#register-spinner').fadeOut('fast','linear',function(){
+                                action: function () {
+                                    $('#register-spinner').fadeOut('fast', 'linear', function () {
                                         $('#register-btn').fadeIn();
                                     });
                                 }
@@ -104,9 +104,9 @@ function reg_user(){
                         }
                     });
                 }
-        });
+            });
 
-    }else{
+    } else {
         $("#register-btn-input-error").show();
         $("#register-btn").removeClass("btn-type-1");
         $("#register-btn").addClass("btn-type-1-error");
