857c52813deaa92137b1d75ed06d306cef407132
Yasiru Randeepa <yasirurandeepa.15@cse.mrt.ac.lk>
2019-01-24 18:07:06 +0530

    side bar

--- Clinical Management System/pages/js/custom/doctor.js
+++ Clinical Management System/pages/js/custom/doctor.js
@@ -1,6 +1,31 @@
 <!-- Navigation Functions -->
-function viewPatient(){
+
+function viewHome(id){
+    addRemoveClass(id);
+    removeSection();
+    const x = document.getElementById("dashboard");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function viewPatient(id){
+    addRemoveClass(id);
+    removeSection();
     const x = document.getElementById("viewPatients");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function viewSchedule(id){
+    addRemoveClass(id);
+    removeSection();
+    const x = document.getElementById("viewSchedule");
     if (x.style.display === "none") {
         x.style.display = "block";
     } else {
@@ -15,4 +40,22 @@ function showResetForm() {
     } else {
         x.style.display = "none";
     }
+}
+
+function addRemoveClass(x) {
+    $("#home").removeClass("active");
+    $("#patient").removeClass("active");
+    $("#schedule").removeClass("active");
+    $("#reset").removeClass("active");
+    $(x).addClass("active");
+}
+
+function removeSection(){
+    const dashboard = document.getElementById("dashboard");
+    const schedule = document.getElementById("viewSchedule");
+    const patients = document.getElementById("viewPatients");
+
+    dashboard.style.display = "none";
+    schedule.style.display = "none";
+    patients.style.display = "none";
 }
