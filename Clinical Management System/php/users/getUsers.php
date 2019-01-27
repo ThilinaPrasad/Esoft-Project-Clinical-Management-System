59ecc9eb5a324747778da3f763ee1cb065ced951
ThilinaPrasad <thilinaprasad.15@cse.mrt.ac.lk>
2019-01-27 17:23:28 +0530

    Patient registration flow midified with feedback

--- Clinical Management System/php/users/getUsers.php
+++ Clinical Management System/php/users/getUsers.php
@@ -6,17 +6,17 @@
  * Time: 12:04 PM
  */
 require_once "../dbOps/crud.php";
-
+$table = $_GET["table"];
 $column = $_GET["column"];
-$data = $_GET["data"];
+$data = $_GET["value"];
 $query = "";
 
 switch ($column) {
     case 'email':
-        $query = "SELECT * FROM users WHERE  $column  = '$data'";
+        $query = "SELECT * FROM ".$table." WHERE  $column  = '$data'";
         break;
     default:
-        $query = "SELECT * FROM users";
+        $query = "SELECT * FROM ". $table;
         break;
 }
 
