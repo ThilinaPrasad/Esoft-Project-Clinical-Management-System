29d3978fe63d4dd4b2aed5cbfb8341675ec43012
ThilinaPrasad <thilinaprasad.15@cse.mrt.ac.lk>
2019-01-30 20:13:38 +0530

    no message

---
+++ Clinical Management System/pages/php/forgotPassValidation.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: thili
+ * Date: 1/30/2019
+ * Time: 7:45 PM
+ */
---
+++ Clinical Management System/pages/php/getAppointmentsBySchId.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: thili
+ * Date: 1/30/2019
+ * Time: 5:48 PM
+ */
