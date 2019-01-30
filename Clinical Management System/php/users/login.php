cbc43bef9871078ec6f70ffb37be70eddad01e26
ThilinaPrasad <thilinaprasad.15@cse.mrt.ac.lk>
2019-01-31 01:09:48 +0530

    Login credential error done

--- Clinical Management System/php/users/login.php
+++ Clinical Management System/php/users/login.php
@@ -4,12 +4,11 @@ require_once "userTypes.php";
 
 $email = $_GET['email'];
 $password = $_GET['password'];
-
 $query = "SELECT id,password,type FROM user WHERE email='$email'";
 $crud = new Crud();
 $response = $crud->getData($query);
 session_start();
-if (password_verify($password, $response[0]['password'])) {
+if ($response && password_verify($password, $response[0]['password'])) {
     $_SESSION["logged_id"] = $response[0]['id'];
     $_SESSION["logged_type"] = $response[0]['type'];
     echo json_encode(array('id'=>$response[0]['id'],'type'=>$response[0]['type']));
